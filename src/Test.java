ce0a0ecf1293fd35f3a29613aea845a3e6f202f8
stanislav3316 <stanislav3316@gmail.com>
2016-10-13 01:46:22 +0300

    now i can get yandex  Cookie

--- src/Test.java
+++ src/Test.java
@@ -1,67 +1,38 @@
-import java.io.BufferedReader;
-import java.io.FileWriter;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by iters on 10/13/16.
  */
 public class Test {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws Exception {
+        new Test().sendGet();
     }
 
     private void sendGet() throws Exception {
-        String url = "https://www.yandex.ru/";
+        String url = "https://yandex.ru/";
         URL obj = new URL(url);
         HttpURLConnection con = (HttpURLConnection) obj.openConnection();
 
-        // optional default is GET
-        con.setRequestMethod("GET");
+        String line;
+        BufferedReader fileReader = new BufferedReader(new FileReader("./requests/getCookie"));
 
-        //add request header
-        con.setRequestProperty("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
-        con.setRequestProperty("Accept-Encoding", "UTF-8");
-        con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
-        con.setRequestProperty("Cache-Control", "max-age=0");
-        con.setRequestProperty("Connection", "keep-alive");
-        con.setRequestProperty("Host", "www.yandex.ru");
-        con.setRequestProperty("Referer", "https://yandex.ru/search/?text=%D0%BA%D0%B0%D0%BA%20%D0%BE%D0%B1%D0%BE%D0%B9%D1%82%D0%B8%20%D0%BA%D0%B0%D0%BF%D1%87%D1%83%20%D1%8F%D0%BD%D0%B4%D0%B5%D0%BA%D1%81%D0%B0&lr=2");
-        con.setRequestProperty("Upgrade-Insecure-Requests", "1");
-        con.setRequestProperty("User-Agent", "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:49.0) Gecko/20100101 Firefox/49.0");
+        con.setRequestMethod("GET");
+        while ((line = fileReader.readLine()) != null) {
+            String header = line.substring(0, line.indexOf(":"));
+            String value = line.substring(line.indexOf(":") + 1, line.length());
+            con.setRequestProperty(header, value);
+        }
 
         int responseCode = con.getResponseCode();
-        System.out.println("\nSending 'GET' request to URL : " + url);
-        System.out.println("Response Code : " + responseCode);
-        System.out.println(con.getResponseMessage());
-        System.out.println(con.getHeaderField(0));
-        System.out.println(con.getHeaderField(1));
-        System.out.println(con.getHeaderField(2));
-        System.out.println(con.getHeaderField(3));
-        System.out.println(con.getHeaderField(4));
-        System.out.println(con.getHeaderField(5));
-        System.out.println(con.getHeaderField(6));
-        System.out.println(con.getHeaderField(7));
-        System.out.println(con.getHeaderField(8));
-        System.out.println(con.getHeaderField(9));
-        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-        String inputLine;
+        System.out.println("request code = " + responseCode);
 
-        StringBuffer response = new StringBuffer();
-        PrintWriter pw = new PrintWriter(new FileWriter("index.txt"));
-
-        while ((inputLine = in.readLine()) != null) {
-            response.append(inputLine);
-            pw.write(inputLine);
+        Map<String, List<String>> responseHeader =  con.getHeaderFields();
+        for (Map.Entry<String, List<String>> entry : responseHeader.entrySet()) {
+            System.out.println(entry.getKey() + " : " + entry.getValue());
         }
-        in.close();
-        pw.close();
-        //print result
-        //System.out.println(response.toString());
-        //System.out.println(response.toString());
-        //System.out.println(response.toString());
-
     }
 }
