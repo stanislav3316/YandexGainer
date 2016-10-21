22d08830b2d520655ee90de1e5b25f621dde3e42
stanislav3316 <stanislav3316@gmail.com>
2016-10-21 19:09:57 +0300

    enter data to file : title/url

--- src/Parsers/UrlParser.java
+++ src/Parsers/UrlParser.java
@@ -7,17 +7,61 @@ import java.util.regex.Pattern;
  * Created by iters on 10/17/16.
  */
 public class UrlParser {
-    public String getLink(String html, int num) {
+    public SearchNode getLink(String html, int num) {
         String result = "";
-        Pattern p = Pattern.compile("<div class=\"a11y-hidden\">" + num + "</div>");
+        String link = "";
+        String find = "<div class=\"a11y-hidden\">" + num + "</div>";
+
+        Pattern p = Pattern.compile(find);
         Matcher m = p.matcher(html);
 
         if (m.find()) {
             int pos = m.start();
-            String block = html.substring(pos, );
+            String block = html.substring(pos + find.length(), html.length());
+            result = block.substring(block.indexOf("</span>"), block.indexOf("</a>"));
         }
 
-        return result;
+        find = "<a class=\"link link_minor_yes sitelinks__link\" target=\"_blank\"";
+        m.reset(html);
+
+        if (m.find()) {
+            int pos = m.start();
+            String block = html.substring(pos + find.length(), html.length());
+            link = block.substring(block.indexOf("href"), block.indexOf("onmousedown") );
+        }
+
+        SearchNode node = new SearchNode(link, result);
+        node = urlHandler(node);
+        return node;
     }
 
+    private SearchNode urlHandler(SearchNode node) {
+        String link = node.link;
+        link = link.substring(link.indexOf("\"") + 1, link.length() - 2);
+
+        String title = node.title;
+        title = title.substring(title.indexOf("</span>") + "</span>".length(), title.length());
+        title = title.replaceAll("</b>", "");
+        title = title.replaceAll("\\.\\.\\.", "");
+        title = title.replaceAll("<b class=\"needsclick\">", "");
+
+        node.title = title;
+        node.link = link;
+        return node;
+    }
+
+    class SearchNode {
+        String link;
+        String title;
+
+        public SearchNode(String link, String title) {
+            this.title = title;
+            this.link = link;
+        }
+
+        @Override
+        public String toString() {
+            return link + "\n" + title;
+        }
+    }
 }
--- src/Test.java
+++ src/Test.java
@@ -1,13 +1,21 @@
+import Parsers.UrlParser;
 import Search.YandexSearch;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.URL;
 
 /**
  * Created by iters on 10/16/16.
  */
 public class Test {
     public static void main(String[] args) throws IOException {
-        System.out.println("<div class=\"a11y-hidden\">1</div>");
-        System.out.println(new YandexSearch().getSearchResult("hello my name is Stanislav"));
+        UrlParser parser = new UrlParser();
+        String search = new YandexSearch().getSearchResult("фильмы");
+        PrintWriter pw = new PrintWriter("out.txt");
+        for (int i = 1; i < 10; i++) {
+            pw.write(String.valueOf(parser.getLink(search, i)) + "\n\n");
+        }
+        pw.close();
     }
 }
